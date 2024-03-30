08b38bf84bf6d45439a2302fd61bedcca4d4f73d
unknown <lokeshalli1807@gmail.com>
2024-03-30 18:26:00 +0530

    Implemented NavBar and Search bar with basic design and also changed color palette

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // importing all the required packages...
 import express from 'express';
 import axios from 'axios';
+import bodyParser from 'body-parser';
 
 // initializing port number
 const port = 3000;
@@ -10,6 +11,9 @@ const app = express();
 
 // declaring public folder as static with experss
 app.use(express.static('public'));
+
+// intializing body-parser middleware
+app.use(bodyParser.urlencoded({extended: true}));
 
 // initializing the apikey with my own API key
 const apiKey  = 'KobCCfAWx3Enygj1pIb6vGx03WxNb8R4';
@@ -21,58 +25,66 @@ const baseUrl = 'https://api.nytimes.com/svc/mostpopular/v2/';
 app.get('/',async (req, res) => {
     // res.render('index.ejs');
 
-    // try{
-    //     const response = await axios.get('https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=KobCCfAWx3Enygj1pIb6vGx03WxNb8R4');
-    //     // res.render('index.ejs', {data: response.data});
-    //     res.send(data);
-    // } catch(err){
-    //     console.error(err.response);
-    // }
+    try{ // if get's an error as too many requests then execute following code and comment this out
+        const response = await axios.get('https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=KobCCfAWx3Enygj1pIb6vGx03WxNb8R4');
+        res.render('index.ejs', {data: response.data});
+        // res.send(data);
+    } catch(err){
+        console.error(err.response);
+    }
 
-    // const axios = require('axios');
 
-    const makeRequestWithRetry = async (url, maxRetries = 3, delay = 1000) => {
-        for (let i = 0; i < maxRetries; i++) {
-            try {
-                const response = await axios.get(url);
-                return response.data;
-            } catch (error) {
-                if (error.response && error.response.status === 429) {
-                    // Rate limit exceeded, wait for some time before retrying
-                    await new Promise(resolve => setTimeout(resolve, delay));
-                } else {
-                    // Other types of errors, throw the error
-                    throw error;
-                }
-            }
-        }
-        throw new Error('Max retries reached. Unable to make successful request.');
-    };
-    // let content;
-    // Usage
-    makeRequestWithRetry(`${baseUrl}emailed/7.json?api-key=${apiKey}`)
-        .then(data => res.render('index.ejs', {data: data}))
-        .catch(error => console.error(error));
-    
-    // res.render('index.ejs', {data: JSON.stringify(content)});
+    // const makeRequestWithRetry = async (url, maxRetries = 3, delay = 1000) => {
+    //     for (let i = 0; i < maxRetries; i++) {
+    //         try {
+    //             const response = await axios.get(url);
+    //             return response.data;
+    //         } catch (error) {
+    //             if (error.response && error.response.status === 429) {
+    //                 // Rate limit exceeded, wait for some time before retrying
+    //                 await new Promise(resolve => setTimeout(resolve, delay));
+    //             } else {
+    //                 // Other types of errors, throw the error
+    //                 throw error;
+    //             }
+    //         }
+    //     }
+    //     throw new Error('Max retries reached. Unable to make successful request.');
+    // };
+
+    // makeRequestWithRetry(`${baseUrl}emailed/7.json?api-key=${apiKey}`)
+    //     .then(data => res.render('index.ejs', {data: data}))
+    //     .catch(error => console.error(error));
+
         
 });
 
 // post at /search with user specified type and period
 app.post('/search',async (req, res) => {
-    // const type = req.body.type; 
-    // const period = req.body.period;
-    // let sharedOnFB = '';
-    
-    // if (req.body.sharedOnFB) { //checking if the user is wanted most shared artiles on facebook or not
-    //     sharedOnFB = '/facebook';
-    // }
+    const type = req.body.type; 
+    const period = req.body.period;
+    let sharedOnFB = '';
 
-    // try{
-    //     const response = await axios.get(baseUrl + type + '/' + period + sharedOnFB + '.json?api-key=' + apiKey);
-    // } catch(err){
-    //     console.error(err.response);
-    // }
+    // const options = {
+    //     type:type,
+    //     period: period,
+    //     sharedOnFB: req.body.sharedOnFB
+    // };
+    
+    if (req.body.sharedOnFB) { //checking if the user is wanted most shared artiles on facebook or not
+        sharedOnFB = '/facebook';
+    }
+
+    // console.log(baseUrl + type + '/' + period + sharedOnFB + '.json?api-key=' + apiKey);
+
+    // res.redirect('/');
+
+    try{
+        const response = await axios.get(baseUrl + type + '/' + period + sharedOnFB + '.json?api-key=' + apiKey);
+        res.render('index.ejs', {data: response.data});
+    } catch(err){
+        console.error(err.response);
+    }
 
 });
 
